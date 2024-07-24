4a3ff5028a7f335b4877aedcfea86c5f5e046b11
Ola Ajibode <ola.ajibode@gmail.com>
2024-07-24 09:59:57 +0000

    add Post model to register

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,9 @@ from django.contrib import admin
 from blog.models import Tag, Post
 
 # Register your models here.
-# Still need to fix a bug here
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ('slug', 'published_at')
 
 admin.site.register(Tag)
-admin.site.register(PostAdmin)
+admin.site.register(Post, PostAdmin)
