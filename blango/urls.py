ef5977ea12c7b2d52a28c6d9607e2b79578d2a9a
Ola Ajibode <ola.ajibode@gmail.com>
2024-07-24 12:25:35 +0000

    Finish html frameworks

--- blango/urls.py
+++ blango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import blog.views
+
 urlpatterns = [
+    path("", blog.views.index),
     path("admin/", admin.site.urls),
 ]
