2d17a1d29fe6d68cc64367d5919925dd07b2fe74
Ola Ajibode <ola.ajibode@gmail.com>
2024-07-24 11:00:31 +0000

    use genericrelations post comment

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,8 @@ from blog.models import Tag, Post
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ('slug', 'published_at')
+    list_display = ("slug", "published_at")
+
 
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -15,25 +15,47 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Tag',
+            name="Tag",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('value', models.TextField(max_length=100)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("value", models.TextField(max_length=100)),
             ],
         ),
         migrations.CreateModel(
-            name='Post',
+            name="Post",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('modified_at', models.DateTimeField(auto_now=True)),
-                ('published_at', models.DateTimeField(blank=True, null=True)),
-                ('title', models.TextField(max_length=100)),
-                ('slug', models.SlugField()),
-                ('summary', models.TextField(max_length=500)),
-                ('content', models.TextField()),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL)),
-                ('tags', models.ManyToManyField(related_name='posts', to='blog.Tag')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created_at", models.DateTimeField(auto_now_add=True)),
+                ("modified_at", models.DateTimeField(auto_now=True)),
+                ("published_at", models.DateTimeField(blank=True, null=True)),
+                ("title", models.TextField(max_length=100)),
+                ("slug", models.SlugField()),
+                ("summary", models.TextField(max_length=500)),
+                ("content", models.TextField()),
+                (
+                    "author",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
+                ("tags", models.ManyToManyField(related_name="posts", to="blog.Tag")),
             ],
         ),
     ]
