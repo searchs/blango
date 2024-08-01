8f746429cb14763d5e38559469c9e558be71d648
Ola Ajibode <ola.ajibode@gmail.com>
2024-08-01 09:54:11 +0000

    Finish django registration

--- blango/urls.py
+++ blango/urls.py
@@ -18,11 +18,21 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
+from django_registration.backends.activation.views import RegistrationView
+from blango_auth.forms import BlangoRegistrationForm
+
 import blog.views
 import blango_auth.views
 
 
 urlpatterns = [
+    path(
+        "accounts/register/",
+        RegistrationView.as_view(form_class=BlangoRegistrationForm),
+        name="django_registration_register",
+    ),
+    path("accounts/", include("django_registration.backends.activation.urls")),
+
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path("", blog.views.index),
