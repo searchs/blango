ab27d2d44b8bc944cb584800977b46a0d80b4f4e
Ola Ajibode <ola.ajibode@gmail.com>
2024-08-01 11:34:37 +0000

    allauth config in settings

--- blango/settings.py
+++ blango/settings.py
@@ -56,12 +56,17 @@ class Dev(Configuration):
         "django.contrib.contenttypes",
         "django.contrib.sessions",
         "django.contrib.messages",
+        "django.contrib.sites",
         "django.contrib.staticfiles",
        
         "blog",
         "crispy_forms",
         "crispy_bootstrap5",
         "debug_toolbar",
+        "allauth", 
+        "allauth.account", 
+        "allauth.socialaccount", 
+        "allauth.socialaccount.providers.google",
     ]
 
     MIDDLEWARE = [
@@ -163,6 +168,14 @@ class Dev(Configuration):
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
     REGISTRATION_OPEN = True
+
+    SITE_ID = 1
+
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+
     
 
 
